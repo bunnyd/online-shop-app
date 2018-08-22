6622eda2be99d122a12dfd3b0411389a8dd4296d
github username <github email address>
2018-08-22 13:16:23 -0500

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users do
-    resources :carts, only: [:index, :edit, :update]
-  end
-  resources :users, :path => 'sellers', as: 'sellers' do
-    resources :inventories, :path => 'store', only: [:show, :index]
-  end
-  resources :sellers do
-    resources :inventories, only: [:index] do
-      resources :products, only: [:index]
-    end
+    resources :carts, only: [:show, :edit, :update]
   end
 
-  # resources :products
+  resources :inventories, :path => 'stores', only: [:show, :index] do
+    resources :products, only: [:index, :show]
+  end
+
+  resources :sellers do
+    resources :inventories, only: [:index, :edit, :destroy] do
+      resources :products, only: [:new, :show, :edit, :destroy]
+    end
+  end
 end
