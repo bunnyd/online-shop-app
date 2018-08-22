3b98c57f3f8f211285a15e8c0b2f16a75dc91710
github username <github email address>
2018-08-22 12:34:31 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :users
+  resources :users do
+    resources :carts, only: [:index, :edit, :update]
+  end
+  resources :users, :path => 'sellers', as: 'sellers' do
+    resources :inventories, :path => 'store', only: [:show, :index]
+  end
+  resources :sellers do
+    resources :inventories, only: [:index] do
+      resources :products, only: [:index]
+    end
+  end
+
+  # resources :products
 end
