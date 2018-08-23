2071065521e2a5d5d1d0515fbdf74e6e28f2584d
github username <github email address>
2018-08-23 17:20:21 -0500

    fix conflicts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
   def redirect_if_not_logged_in
     if !session[:user_id]
       redirect_to login_path
@@ -9,7 +8,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @user = User.find(session[:user_id])
   end
-=======
-
->>>>>>> erika
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,8 @@ class User < ApplicationRecord
   has_many :cart_products, through: :cart
   has_many :products, through: :cart
   has_one :inventory, required: false, foreign_key: :seller_id
-<<<<<<< HEAD
-  # has_secure_password
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-=======
+
   has_secure_password
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable
 
   validates :email, :username, :first_name, :last_name, :credit_card_number, presence: true
   validates :credit_card_number, length: { is: 16 }
@@ -18,6 +12,4 @@ class User < ApplicationRecord
   def full_name
     "#{first_name} #{last_name}"
   end
-
->>>>>>> 54afe5e03fec35edfa547e120690b0b6a90d29ed
 end
