96ca3383e0e4a2c5f7d0225754bdb1f559531766
github username <github email address>
2018-08-23 16:47:14 -0500

    updated cart_products_controller

---
+++ app/controllers/cart_products_controller.rb
@@ -0,0 +1,63 @@
+class CartProductsController < ApplicationController
+  before_action :set_cart_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cart_products = CartProduct.all
+  end
+
+  def show
+    @user = User.find(session[:user_id])
+  end
+
+  def new
+    @cart_product = CartProduct.new
+  end
+
+  def create
+    # byebug
+    @user = User.find(session[:user_id])
+    product = Product.find(params[:product_id])
+    # byebug
+    @cart_product = @user.cart.add_product(product.id)
+    # byebug
+    if @cart_product.valid?
+      @cart_product.save
+      redirect_to @cart_product.cart
+    else
+      flash[:error] = @cart_product.errors.full_messages[0]
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @cart_product.update(cart_product_params)
+      redirect_to @cart_product
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # @cart_product = CartProduct.find(params[:id])
+    @cart_product.destroy
+    byebug
+    redirect_to carts_path
+  end
+
+  private
+  def set_cart_product
+    # byebug
+    @cart_product = CartProduct.find(session[:user_id])
+
+  end
+
+  def cart_product_params
+  # byebug
+  params.require(:cart_product).permit(:product_id)
+  end
+
+end
